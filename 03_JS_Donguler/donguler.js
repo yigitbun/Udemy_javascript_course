f233d9658b7c427c85f964258867be168b5652b0
Bunyamin Yigit <yigitbun@gmail.com>
2021-02-11 23:11:55 +0100

    Klasörler düzenlendi.

---
+++ 03_JS_Donguler/donguler.js
@@ -0,0 +1,45 @@
+        
+    /*
+        //Document Write && Writeln
+
+        document.write("Gerzek ders<br>");
+        document.write("Hala gerzek<br>");
+
+        document.write("Php");
+        document.write("Bootstrap");
+        document.write("JS<br>");
+
+        document.writeln("Php");
+        document.writeln("Bootstrap");
+        document.writeln("JS<br>");
+
+        var s = "Sam Sepiol";
+        document.write(s)
+        
+    */
+
+    /*
+        // Alert ve Promt Kullanimi (body icinde)
+        
+
+        
+        Not: Promt ile istenenler string olarak gelir bu ypzden "parseInt" kullaniriz
+
+        alert("ben ne ise yararim ki")
+        <div id="sonuc"></div>
+        <script type="text/javascript">
+        var sayi1 = prompt("Bir sayi girin");
+        var sayi2 = prompt("Bir sayi girin");
+
+        topla = parseInt(sayi1) + parseInt(sayi2);
+
+        document.getElementById("sonuc").innerHTML = topla;
+
+    */
+
+    
+        // For Döngüsü
+        
+        
+        
+    
