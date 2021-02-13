044ec7a067a3eec380bb8bdf0cc2223f896c3e9e
Bunyamin Yigit <yigitbun@gmail.com>
2021-02-13 01:51:06 +0100

    Completed

--- 03_JS_Donguler/donguler.js
+++ 03_JS_Donguler/donguler.js
@@ -1,5 +1,5 @@
         
-    /*
+/*
         //Document Write && Writeln
 
         document.write("Gerzek ders<br>");
@@ -16,9 +16,9 @@
         var s = "Sam Sepiol";
         document.write(s)
         
-    */
+*/
 
-    /*
+/*
         // Alert ve Promt Kullanimi (body icinde)
         
 
@@ -35,12 +35,12 @@
 
         document.getElementById("sonuc").innerHTML = topla;
 
-    */
+*/
 
-    
+
+/*
+
         // For Döngüsü
-
-    /*
 
         var i;
 
@@ -63,4 +63,116 @@
             document.writeln(i + "<br>");
 
         }
-    */
+
+
+
+*/
+/* 
+
+        // While Döngüsü
+
+        var i = 0;
+
+        while (i<10) {
+            document.write(i + ". sayi<br>")
+            i++;
+        } 
+*/
+
+/* 
+
+        //Do while Döngüsü
+
+
+        var i =11;
+        do {
+            document.write("Sayi" + i + "<br>")
+            i++;
+        } while (i < 11);
+ */
+
+        
+ /* 
+        // For in Döngüsü
+
+        var courses = {ccourse1:"Php", course2:"bootstrap"};
+
+        for (key in courses) {
+
+            document.write(courses[key] + "<br>");
+        }
+
+        // Dizi Örnegi
+
+        var egitimler = new Array("php", "bootstrap", "Jquery");
+
+        for (key in egitimler) {
+
+            document.write(egitimler[key] + "<br>")
+        }
+ */
+/* 
+        // Ic ice döngü
+
+        var i, x;
+
+         for (i=1; i<11; i++) {
+
+            for (x=1; x < 11; x++) {
+                document.write(i + "x" + x + "=" + i*x + "<br>")
+            }
+
+            
+
+         }
+
+ */
+
+/* 
+         // Break
+
+         // Önce bir hatirlatma;
+
+
+         var i = 10;
+
+         switch (i) {
+
+
+             case 10:
+             document.writeln("Number is 10 <br>");
+             break;
+
+             case 11:
+             document.writeln("Number is 10");
+             break;
+
+         }
+
+         var i;
+
+        for (i=3; i<=10; i++) {
+            document.writeln(i);
+            if (i==7) {
+                document.writeln("You have reached number 7, stop!");
+                break
+            }
+
+        }
+ */
+
+ 
+/* 
+        // Continue
+
+        var i;
+
+        for (i=3; i<=10; i++) {
+            
+            if (i==7) {
+                continue;
+            }
+            document.writeln(i);
+        }
+
+ */
